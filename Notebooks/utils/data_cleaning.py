03c7f03c90b842eff058d7c1b5f9ac3e196a5cd6
Paul Savala <paulsavala@gmail.com>
2020-12-18 11:08:22 -0600

    Data fixes

--- Notebooks/utils/data_cleaning.py
+++ Notebooks/utils/data_cleaning.py
@@ -65,3 +65,59 @@ def optimize(df):
     df = to_numeric(df)
     df = downcast(df)
     return df
+
+
+def uniform_name(x):
+    raise_errors = False
+    try:
+        x = x.strip().lower()
+    except Exception as e:
+        print(f'{x} is invalid')
+        return x
+        if raise_errors:
+            raise
+        
+    team_list = ['TOR', 'SFN', 'SEA', 'NYA', 'LAN', 'BAL', 'COL', 'CHN', 'MIA',
+       'CLE', 'CIN', 'TEX', 'TBA', 'DET', 'ATL', 'HOU', 'MIL', 'WAS',
+       'PHI', 'OAK', 'ARI', 'CHA', 'BOS', 'KCA', 'SLN', 'PIT', 'NYN',
+       'MIN', 'SDN', 'ANA']
+    
+    
+    
+    team_dict = {'jays':'TOR','bluejays':'TOR', 'torontobluejays':'TOR', 'tor':'TOR', 
+                 'giants':'SFN', 'sfg': 'SFN', 'sanfranciscogiants':'SFN', 'sf':'SFN', 'sfn':'SFN', 
+                 'mariners':'SEA', 'seattlemariners':'SEA', 'sea':'SEA', 
+                 'yankees':'NYA', 'nyy': 'NYA', 'newyorkyankees':'NYA', 'nya':'NYA', 
+                 'dodgers':'LAN', 'lad':'LAN', 'losangelesdodgers':'LAN', 'la':'LAN', 'lan':'LAN', 
+                 'orioles':'BAL', 'baltimoreorioles':'BAL', 'bal':'BAL', 
+                 'rockies':'COL', 'coloradorockies':'COL', 'col':'COL', 
+                 'cubs':'CHN', 'chc':'CHN', 'chicagocubs':'CHN', 'chn':'CHN', 
+                 'marlins':'MIA', 'fla':'MIA', 'floridamarlins':'MIA', 'miamimarlins':'MIA', 'mia':'MIA', 
+                 'indians':'CLE', 'clevelandindians':'CLE', 'cle':'CLE', 
+                 'reds':'CIN', 'cincinnatireds':'CIN', 'cin':'CIN', 
+                 'rangers':'TEX', 'texasrangers':'TEX', 'tex':'TEX', 
+                 'rays':'TBA', 'devilrays':'TBA', 'tbd':'TBA', 'tampabayrays':'TBA', 'tampabaydevilrays':'TBA', 'tba':'TBA', 'tbr': 'TBA',
+                 'tigers':'DET', 'detriottigers':'DET', 'det':'DET', 
+                 'braves':'ATL', 'atlantabraves':'ATL', 'atl':'ATL', 
+                 'astros':'HOU', 'houstonastros':'HOU', 'hou':'HOU', 
+                 'brewers':'MIL', 'milwaukeebrewers':'MIL', 'mil':'MIL', 
+                 'nationals':'WAS', 'wsh':' WAS', 'wsn':'WAS', 'washingtonnationals':'WAS', 'montrealexpos':'WAS', 'expos':'WAS', 'mtl':'WAS','was':'WAS', 'mon': 'WAS',
+                 'phillies':'PHI', 'philadelphiaphillies':'PHI', 'phi':'PHI', 
+                 'as':'OAK', 'athletics':'OAK', 'oaklandathletics':'OAK', 'oaklandas':'OAK', 'oaklanda':'OAK', 'oak':'OAK', 
+                 'diamondbacks':'ARI', 'arizonadiamondbacks':'ARI', 'ari':'ARI', 
+                 'whitesox':'CHA', 'cws':'CHA', 'chicagowhitesox':'CHA', 'cha':'CHA', 'chw': 'CHA', 'chw': 'CHA',
+                 'redsox':'BOS', 'bostonredsox':'BOS', 'bos':'BOS', 
+                 'royals':'KCA', 'kcr':'KCA', 'kansascityroyals':'KCA', 'kc':'KCA', 'kca':'KCA', 
+                 'cardinals':'SLN', 'slc':'SLN', 'stl':'SLN', 'saintlouiscardinals':'SLN', 'stlouiscardinals':'SLN', 'sln':'SLN', 
+                 'pirates':'PIT', 'pittsburghpirates':'PIT', 'pit':'PIT', 
+                 'mets':'NYN', 'nym':'NYN', 'newyorkmets':'NYN', 'nyn':'NYN', 
+                 'twins':'MIN', 'minnesotatwins':'MIN', 'min':'MIN', 
+                 'padres':'SDN', 'sdp':'SDN', 'sandiegopadres':'SDN', 'sd':'SDN', 'sdn':'SDN', 
+                 'angels':'ANA', 'laa':'ANA', 'losangelesangels':'ANA', 'losangelesangelsofanaheim':'ANA', 'ana':'ANA', 
+                '---': 'UNK'}
+    if x not in team_dict:
+        if raise_errors:
+            assert x in team_dict, f'{x} is not a valid team name'
+        else:
+            print(f'{x} is not a valid team name')
+    return team_dict.get(x, x)
